c0fb5728c3e918b51c35d9d8b40cdcb7a6ebe227
Andrew Bowers <abowers@geo-blue.com>
2017-08-17 08:41:05 -0400

    Added Sample Controller

---
+++ BaseASPCore/BaseASPCore/Controllers/SampleController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseASPCore.Controllers
+{
+    public class SampleController : Controller
+    {
+        public SampleController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
